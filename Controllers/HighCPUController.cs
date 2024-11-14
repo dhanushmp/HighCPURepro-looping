42bcc67f2d5eeddb88188aa316aa189e195c9617
Dhanush M P <dhanush.mp@outlook.com>
2024-11-14 22:01:03 +0530

    first commit

---
+++ Controllers/HighCPUController.cs
@@ -0,0 +1,30 @@
+﻿using High_CPU.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace High_CPU.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HighCPUController : Controller
+    {
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateValue([FromBody] HighCPUdetails HighCPU)
+        {
+            string name  = HighCPU.Value;
+            int count = 0;
+            if (name != null)
+            {
+                for (int i = 0; i < 60000; i++)
+                {
+                   name += "Mindtree";
+                    count++;
+                }
+                
+
+            }
+            return Ok(name);
+        }
+    }
+}
---
+++ Models/HighCPUdetails.cs
@@ -0,0 +1,7 @@
+﻿namespace High_CPU.Models
+{
+    public class HighCPUdetails
+    {
+        public string? Value { get; set; }
+    }
+}
